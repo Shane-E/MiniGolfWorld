5eb5d7e95840ab32f4c818fcf00b0ba6987f5085
Shane Eckenrode <sue2@psu.edu>
2016-02-09 03:18:51 -0500

    Added camera zoom to mouse scroll. Started code for stopping ball from rolling when hit. Added logic to hit ball only when not moving and when mouse is clicked instead of when ball is clicked. Adjusted power levels to find min and max hit power.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -3,18 +3,25 @@ using System.Collections;
 
 public class CameraFollow : MonoBehaviour {
 	public GameObject target = null;
+    public float viewArea;
 	private float yOffset;
 	private float zOffset;
 	private float rotateSpeed;
-	float xRotation;
-	float yRotation;
+	private float xRotation;
+	private float yRotation;
+    private float minView;
+    private float maxView;
+    private float viewChangeAmount;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		yOffset = 2f;
 		zOffset = -3f;
 		rotateSpeed = 2.0f;
 
+        minView = 75.0f;
+        maxView = 125.0f;
+        viewChangeAmount = 30.0f;
 	}
 	
 	// Update is called once per frame
@@ -28,11 +35,19 @@ public class CameraFollow : MonoBehaviour {
             {
                 xRotation += Input.GetAxis("Mouse X") * rotateSpeed;
                 yRotation += Input.GetAxis("Mouse Y") * rotateSpeed;
-                
             }
 
             transform.RotateAround(target.transform.position, Vector3.up, xRotation);
             transform.RotateAround(target.transform.position, Vector3.right, yRotation);
+            //target.transform.Rotate(new Vector3(xRotation, 0, 0));
+
+            //Allow the camera to zoom in and out it's field of view using the scroll wheel.
+            viewArea = Camera.main.fieldOfView;
+            viewArea -= Input.GetAxis("Mouse ScrollWheel") * viewChangeAmount;
+            viewArea = Mathf.Clamp(viewArea, minView, maxView);
+
+            //Set the new view area angle to the main camera.
+            Camera.main.fieldOfView = viewArea;
 
             //Orient camera toward the target.
             transform.LookAt(target.transform);
--- Assets/Scripts/Golfball.cs
+++ Assets/Scripts/Golfball.cs
@@ -3,27 +3,39 @@ using System.Collections;
 
 public class Golfball : MonoBehaviour {
 	public GameObject ball = null;
-	public double ballSpeed = 0;
-	public int power = 0;
+	private bool isMoving = false;
+    private float ballDrag;
+	public int minHitPower = 500;
+    public int maxHitPower = 2750;
 
-	// Use this for initialization
-	void Start () {
-		power = 15;
-	}
+    // Use this for initialization
+    void Start() {
+        ballDrag = ball.GetComponent<Rigidbody>().angularDrag = 1;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        //Allow the ball to be hit if the ball is not null, the ball is not currently moving, and the left mouse button is clicked.
+        if (ball != null && Input.GetMouseButtonDown(0) && !isMoving)
+        {
+            isMoving = true;
+            ball.GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, minHitPower));
+        }
+    }
 
-	void OnMouseDown() {
+	/*void OnMouseDown() {
 		//Power of hit increases
 		if (ball != null) {
-			ball.GetComponent<Rigidbody> ().AddForce (Vector3.forward * 5000);
+			ball.GetComponent<Rigidbody> ().AddForce (new Vector3(0, 0, maxHitPower));
 		}
-	}
+	}*/
 
 	void OnMouseUp() {
 		//Hit ball using power level.
 	}
+
+    void OnCollisionStay()
+    {
+        
+    }
 }
