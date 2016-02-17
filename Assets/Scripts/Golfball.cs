ea497bce200bbe17f9b9e12b24bcb012f44bd97e
Shane Eckenrode <sue2@psu.edu>
2016-02-17 17:54:52 -0500

    Updates to ball shot to stop ball when it is not moving. Fixed power bar to fluctuate between max and min values.

--- Assets/Scripts/Golfball.cs
+++ Assets/Scripts/Golfball.cs
@@ -8,16 +8,19 @@ public class Golfball : MonoBehaviour {
 	public Text distance;
     public Slider powerbar;
 	private bool isMoving = false;
+	private bool increasing = true;
     private float distanceToHole;
-	public float minHitPower = 50.0f;
-    public float maxHitPower = 275.0f;
+	public float minHitPower = 40.0f;
+    public float maxHitPower = 270.0f;
 	private float hitPower = 0;
-	private float powerIncrement = 2.0f;
+	private float powerIncrement = 5.0f;
 	private float powerMultiplier = 10;
+	private float ballRollTime = 0;
 
     // Use this for initialization
     void Start() {
-        distance.GetComponent<Text>().text = "Distance To Hole: " + distanceToHole;
+        distance.GetComponent<Text>().text = "Distance To Hole:" + distanceToHole;
+		ball.GetComponent<Rigidbody> ();
     }
 	
 	// Update is called once per frame
@@ -25,14 +28,22 @@ public class Golfball : MonoBehaviour {
         //Allow the ball to be hit if the ball is not null, not currently moving, and the left mouse button is clicked.
 		if (ball != null) {
 			if (Input.GetButton("Fire1") && !isMoving) {
-				if (hitPower >= maxHitPower) {
-					hitPower -= powerIncrement * powerMultiplier;
-				}else if (hitPower < maxHitPower) {
-					hitPower += powerIncrement * powerMultiplier;
+				calculatePower ();
+			}
+
+			//Debug.Log ("Ball Velocity: " + GetComponent<Rigidbody> ().velocity.magnitude);
+			Debug.Log (isMoving);
+
+			//Detect when the ball stops
+			if (isMoving) {
+				ballRollTime += Time.deltaTime;
+				if (ballRollTime > 1 && GetComponent<Rigidbody> ().velocity.magnitude <= 0.5) {
+					GetComponent<Rigidbody> ().velocity = Vector3.zero;
+					GetComponent<Rigidbody> ().angularVelocity = Vector3.zero;
+					isMoving = false;
 				}
-                //Update the slider
-                powerbar.value = hitPower / powerMultiplier;
-				Debug.Log ("Power: " + hitPower);
+			} else {
+				ballRollTime = 0;
 			}
 
 		}
@@ -42,12 +53,41 @@ public class Golfball : MonoBehaviour {
     }
 
 	void OnMouseUp() {
-		//Hit ball using power level.
+		//Hit ball using power level and set ball to moving.
 		Debug.Log ("Selected Power: " + hitPower);
 		hitBall (hitPower);
+		isMoving = true;
+	}
+
+	void calculatePower(){
+		//Increase power if it is less than the max power.
+		if (increasing) {
+			if (hitPower < maxHitPower) {
+				hitPower += powerIncrement * powerMultiplier;
+				increasing = true;
+			} else if (hitPower >= maxHitPower) {
+				increasing = false;
+			}
+		}
+		//Decrease power if power level is not increasing until the power hits the minimum level.
+		if(!increasing) {
+			Debug.Log ("Not Increasing");
+			if (hitPower > minHitPower) {
+				Debug.Log ("HitPower: " + hitPower);
+				hitPower -= powerIncrement * powerMultiplier;
+			} else if (hitPower <= minHitPower) {
+				increasing = true;
+			}
+		}
+		//Update the slider
+		powerbar.value = hitPower / powerMultiplier;
 	}
 
 	void hitBall (float power){
 		ball.GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, power));
+
+		//Reset the power  and power bar level to minimum default after hitting ball
+		hitPower = minHitPower;
+		powerbar.value = hitPower;
 	}
 }
--- Assets/Scripts/Windmill.cs
+++ Assets/Scripts/Windmill.cs
@@ -4,17 +4,20 @@ using System.Collections;
 public class Windmill : MonoBehaviour {
 
     public GameObject windmillBlade;
+	public float bladeRotateSpeed = 0;
 
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		bladeRotateSpeed = Time.deltaTime * 75;
+
 	    if(windmillBlade != null)
         {
-            transform.Rotate(0, 0, Time.deltaTime * 50, Space.World);
+			transform.Rotate(0, 0, bladeRotateSpeed, Space.World);
         }
 	}
 }
